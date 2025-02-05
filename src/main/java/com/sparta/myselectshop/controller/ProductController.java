3d5b2895902f0399d7c6cf1f63aae3954c7fd136
dvlpsy <adgjl0122@naver.com>
2025-02-05 09:38:49 +0900

    오류 수정2

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -10,8 +10,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -40,4 +38,31 @@ public class ProductController {
         return productService.getProducts(userDetails.getUser(), page-1, size, sortBy, isAsc);
     }
 
+    @PostMapping("/products/{productId}/folder")
+    public void addFolder(
+            @PathVariable Long productId,
+            @RequestParam Long folderId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        productService.addFolder(productId, folderId, userDetails.getUser());
+    }
+
+    @GetMapping("/folders/{folderId}/products")
+    public Page<ProductResponseDto> getProductsInFolder(
+            @PathVariable Long folderId,
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        return productService.getProductsInFolder(
+          folderId,
+          page-1,
+          size,
+          sortBy,
+          isAsc,
+          userDetails.getUser()
+        );
+    }
 }
